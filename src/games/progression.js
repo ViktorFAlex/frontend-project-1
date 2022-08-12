01dc224428de15179a69c3edccb64660c79b3616
ViktorFAlex <viktor.f.alex@gmail.com>
2022-08-12 16:00:28 +0800

    change name of var

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,11 +4,11 @@ import { getRandomNumber, getRandomNumberFromOne } from '../getRandomNumber.js';
 const getProgressionDirection = () => (Math.round(Math.random()) ? 1 : -1);
 
 const getProgressionTask = () => {
-  const elemsInProgression = 10;
+  const progressionElemsCount = 10;
   const start = getRandomNumberFromOne(10);
   const step = getRandomNumberFromOne(10) * (getProgressionDirection());
   const progression = [];
-  for (let i = 1; i <= elemsInProgression; i += 1) {
+  for (let i = 1; i <= progressionElemsCount; i += 1) {
     progression.push(start + step * i);
   }
   const missingNumberIndex = getRandomNumber(progression.length);
